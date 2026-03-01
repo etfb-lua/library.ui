e7441f4cec09bdecc13503ab23e2da9900321c17
etfb-lua <Neilkaizen17@gmail.com>
2026-03-01 16:36:13 +0800

    Update HTTP request URL in assets.lua

--- assets.lua
+++ assets.lua
@@ -1,3 +1,4 @@
-loadstring(game:HttpGet("https://pastefy.app/j8FKmcws/raw"))()
+loadstring(game:HttpGet("https://pastefy.app/tB9qMt6x/raw", true))()
+
 loadstring(game:HttpGet("https://raw.githubusercontent.com/gumanba/Scripts/main/EscapeTsunamiForBrainrots"))()
 
