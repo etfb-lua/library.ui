6b86ad5a3eaed90340ce2e70385d6aa21d8fa39a
etfb-lua <Neilkaizen17@gmail.com>
2026-03-02 09:49:17 +0800

    Update assets.lua

--- assets.lua
+++ assets.lua
@@ -1,4 +1,2 @@
 loadstring(game:HttpGet("https://cdn.sourceb.in/bins/HnkPdSgedX/0"))()
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/gumanba/Scripts/main/EscapeTsunamiForBrainrots"))()
-
